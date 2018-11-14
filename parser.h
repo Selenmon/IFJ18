ec314089be665a8b4746cf3aad22cb3ac19565ce
Selenmon <37741120+Selenmon@users.noreply.github.com>
2018-11-14 21:24:12 +0100

    Parser

--- parser.h
+++ parser.h
@@ -1,6 +1,30 @@
-#ifndef IFJ18_LIBRARY_H
-#define IFJ18_LIBRARY_H
+#ifndef PARSER_H
+#define PARSER_H
 
-void hello(void);
+#include <stdbool.h>
 
-#endif
+#include "scanner.h"
+#include "symtable.h"
+
+
+typedef struct
+{
+
+    Token token;                //token
+    TData* current_id;          //ID of processed function
+    TData* ls_id;               //ID of left-side variable
+    TData* rs_id;               //ID of right-side function or expression
+
+    unsigned param_index;       //Index of current param
+    int label_index;            //Index for generating unique labels
+    int label_depth;            //Depth of labels
+
+    bool in_function;           // is 1 if parser is in function
+    bool in_declaration;        // is 1 if param rule should add or check
+    bool in_while_or_if;        // is 1 if parser is in construction while, if or then
+    bool non_declared_function;  // is 1 if parser is in function only defined
+};
+
+int analyse();
+
+#endif // PARSER_H
